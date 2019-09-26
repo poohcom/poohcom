47d71ec0785c43aac8563c14b1636e1b50a57ded
poohcom <poohcom@naver.com>
2019-09-26 12:00:42 +0900

    aaa

--- docs/DecalV2/app.js
+++ docs/DecalV2/app.js
@@ -1,4 +1,4 @@
-import * as THREE from '../../build/three.module.js';
+import * as THREE from '../build/three.module.js';
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import { FBXLoader } from './jsm/loaders/FBXLoader.js';
 import { DecalGeometry2 } from './jsm/geometries/DecalGeometry2.js';
